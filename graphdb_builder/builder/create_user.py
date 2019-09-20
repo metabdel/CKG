25c350e26a1976e320a2efb9b782c81fec2da591
Rita Colaço <rita.colaco@cpr.ku.dk>
2019-09-20 17:48:05 +0200

    Divided user loading and import. Tested with builder.py, from excel file, and command line.

--- graphdb_builder/builder/create_user.py
+++ graphdb_builder/builder/create_user.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 import argparse
 import pandas as pd
 import numpy as np
@@ -13,9 +14,9 @@ log_config = ckg_config.graphdb_builder_log
 logger = builder_utils.setup_logging(log_config, key='user_creation')
 
 try:
-    config = builder_utils.setup_config('builder')
+	config = builder_utils.setup_config('builder')
 except Exception as err:
-    logger.error("Reading configuration > {}.".format(err))
+	logger.error("Reading configuration > {}.".format(err))
 
 
 cwd = os.path.abspath(os.path.dirname(__file__))
@@ -50,111 +51,145 @@ def check_if_node_exists(driver, node_property, value):
 		query = user_creation_cypher[query_name]['query'].replace('PROPERTY', node_property)
 		for q in query.split(';')[0:-1]:
 			if '$' in q:
-				result = connector.getCursorData(driver, q+';', parameters={'value':str(value)}).values
+				result = connector.getCursorData(driver, q+';', parameters={'value':value})
 			else:
-				result = connector.getCursorData(driver, q+';').values
+				result = connector.getCursorData(driver, q+';')
 	except Exception as err:
 		exc_type, exc_obj, exc_tb = sys.exc_info()
 		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
 		logger.error("Reading query {}: {}, file: {},line: {}, error: {}".format(query_name, sys.exc_info(), fname, exc_tb.tb_lineno, err))
 	return result
 
-def create_user_db(driver, args):
-	query_name_add = 'create_user'
-	query_name_role = 'add_role_to_user'
-	try:
-		cypher = get_user_creation_queries()
-		query = cypher[query_name_add]['query'] + cypher[query_name_role]['query']
-		for q in query.split(';')[0:-1]:
-			if '$' in q:
-				result = connector.getCursorData(driver, q+';', parameters=args)
-			else:
-				result = connector.getCursorData(driver, q+';')
-	except Exception as err:
-		exc_type, exc_obj, exc_tb = sys.exc_info()
-		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		logger.error("Reading query {}: {}, file: {},line: {}, error: {}".format(query_name_add, sys.exc_info(), fname, exc_tb.tb_lineno, err))
-	return 'Done'
-
-def create_user_from_command_line(args, expiration=365):
-	data = vars(args)
-	result = create_user(data, expiration)
-
-	return result
-
-
-def create_user_from_file(filepath, expiration):
-    data = pd.read_excel(filepath).applymap(str)
-    result = create_user(data, expiration)
-    
-    return result
-
-def create_user(data, expiration=365):
+def create_db_user(driver, data):
 	query_name_add = 'create_db_user'
 	query_name_role = 'add_role_to_db_user'
-	query_name_node = 'create_user_node'
 	query_list_db_users =  'list_db_users'
-
-	driver = connector.getGraphDatabaseConnectionConfiguration()
-	date = datetime.today() + timedelta(days=expiration)
-	df = []
-	done = 0
 
 	try:
 		cypher = get_user_creation_queries()
 		db_query = cypher[query_name_add]['query'] + cypher[query_name_role]['query']
+		db_users = connector.getCursorData(driver, cypher[query_list_db_users]['query'],{})
+		if data['username'] not in db_users['username'].to_list() or db_users.empty:
+			for q in db_query.split(';')[0:-1]:
+				result = connector.getCursorData(driver, q+';', parameters=data.to_dict())
+			logger.info("New user created: {}. Result: {}".format(data['username'], result))
+	except Exception as err:
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+		logger.error("Reading query {}: {}, file: {},line: {}, error: {}".format(query_name_add, sys.exc_info(), fname, exc_tb.tb_lineno, err))
+
+def create_user_node(driver, data):
+	query_name_node = 'create_user_node'
+	try:
+		cypher = get_user_creation_queries()
+		query = cypher[query_name_node]['query']
+		for q in query.split(';')[0:-1]:
+			result = connector.getCursorData(driver, q+';', parameters=data.to_dict())
+		logger.info("New user node created: {}. Result: {}".format(data['username'], result))
+	except Exception as err:
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+		logger.error("Reading query {}: {}, file: {},line: {}, error: {}".format(query_name_add, sys.exc_info(), fname, exc_tb.tb_lineno, err))
+
+
+#To use with builder.py
+def extractUsersInfo(filepath, expiration=365):
+	data = pd.read_excel(filepath).applymap(str)
+	driver = connector.getGraphDatabaseConnectionConfiguration()
+	date = datetime.today() + timedelta(days=expiration)
+	df = []
+	try:
+		user_identifier = get_new_user_identifier(driver)
+		if user_identifier is None:
+			user_identifier = 'U1'
+		new_id = int(re.search('\d+', user_identifier).group())
+
 		for index, row in data.iterrows():
 			username = check_if_node_exists(driver, 'username', row['username'])
 			name = check_if_node_exists(driver, 'name', row['name'])
 			email = check_if_node_exists(driver, 'email', row['email'])
-			if len(username) == 0 and len(name) == 0 and len(email) == 0:
-				user_id = get_new_user_identifier(driver)
-				if user_id is None:
-					user_id = 'U1'
-				row['ID'] = user_id
+			if username.empty and name.empty and email.empty:
+				row['ID'] = 'U{}'.format(new_id)
 				row['acronym'] = ''.join([c for c in row['name'] if c.isupper()])
 				row['password'] = row['username']
 				row['rolename'] = 'reader'
 				row['expiration_date'] = date.strftime('%Y-%m-%d')
 				row['image'] = ''
-				
-				for q in cypher[query_name_node]['query'].split(';')[0:-1]:
-					parameters = {}
-					if '$' in q:
-						parameters = row.to_dict()
-					result = connector.getCursorData(driver, q+';', parameters=parameters)
-				logger.info("New user created: {}. Result: {}".format(row['username'], result))
 				df.append(row)
-				done +=1
-				db_users = connector.getCursorData(driver, cypher[query_list_db_users]['query'],{})
-				if row['username'] not in db_users['username'].to_list() or db_users.empty:
-					print("IN")
-					for q in db_query.split(';')[0:-1]:
-						result = connector.getCursorData(driver, q+';', parameters=row.to_dict())
+				create_db_user(driver, row)
+			new_id += 1
+
 	except Exception as err:
 		exc_type, exc_obj, exc_tb = sys.exc_info()
 		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-		logger.error("Reading query {}: {}, file: {},line: {}, error: {}".format(query_name_add, sys.exc_info(), fname, exc_tb.tb_lineno, err))
-
+		logger.error("Extracting users info: {}, file: {},line: {}".format(sys.exc_info(), fname, exc_tb.tb_lineno))
 	if len(df) > 0:
-		#Save new user to file
-		usersDir = os.path.join(cwd,'../../../data/imports/users')
-		ifile = os.path.join(usersDir, 'users.tsv')
 		data = pd.DataFrame(df)
 		data = data[['ID', 'acronym', 'name', 'username', 'email', 'secondary_email', 'phone_number', 'affiliation', 'expiration_date', 'rolename', 'image']]
+		GenerateGraphFiles(data)
 
-		if os.path.exists(ifile):
-			with open(ifile, 'a') as f:
-				data.to_csv(path_or_buf = f, sep='\t',
-							header=False, index=False, quotechar='"',
-							line_terminator='\n', escapechar='\\')
-		else:
-			with open(ifile, 'w') as f:
-				data.to_csv(path_or_buf = f, sep='\t',
-							header=True, index=False, quotechar='"',
-							line_terminator='\n', escapechar='\\')
-	return done
-    
+
+#To use outside builder.py
+def create_user_from_command_line(args, expiration):
+	data = vars(args)
+	data = pd.DataFrame.from_dict(data, orient='index').T
+	result = create_user(data, expiration)
+	return result
+
+
+def create_user_from_file(filepath, expiration):
+	data = pd.read_excel(filepath).applymap(str)
+	result = create_user(data, expiration)    
+	return result
+
+def create_user(data, expiration=365):
+	driver = connector.getGraphDatabaseConnectionConfiguration()
+	date = datetime.today() + timedelta(days=expiration)
+	df = []
+
+	try:
+		for index, row in data.iterrows():
+			username = check_if_node_exists(driver, 'username', row['username'])
+			name = check_if_node_exists(driver, 'name', row['name'])
+			email = check_if_node_exists(driver, 'email', row['email'])
+			if username.empty and name.empty and email.empty:
+				user_identifier = get_new_user_identifier(driver)
+				if user_identifier is None:
+					user_identifier = 'U1'
+				row['ID'] = user_identifier
+				row['acronym'] = ''.join([c for c in row['name'] if c.isupper()])
+				row['password'] = row['username']
+				row['rolename'] = 'reader'
+				row['expiration_date'] = date.strftime('%Y-%m-%d')
+				row['image'] = ''
+				create_user_node(driver, row)
+				create_db_user(driver, row)
+				df.append(row)
+	except Exception as err:
+		exc_type, exc_obj, exc_tb = sys.exc_info()
+		fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+		logger.error("Creating users: file: {},line: {}, error: {}".format(sys.exc_info(), fname, exc_tb.tb_lineno, err))
+	
+	if len(df) > 0:
+		data = pd.DataFrame(df)
+		data = data[['ID', 'acronym', 'name', 'username', 'email', 'secondary_email', 'phone_number', 'affiliation', 'expiration_date', 'rolename', 'image']]
+		GenerateGraphFiles(data)
+
+def GenerateGraphFiles(data):
+	importDir = os.path.join(cwd,'../../../data/imports/users')
+	ckg_utils.checkDirectory(importDir)
+	ifile = os.path.join(importDir, 'users.tsv')
+
+	if os.path.exists(ifile):
+		with open(ifile, 'a') as f:
+			data.to_csv(path_or_buf = f, sep='\t',
+						header=False, index=False, quotechar='"',
+						line_terminator='\n', escapechar='\\')
+	else:
+		with open(ifile, 'w') as f:
+			data.to_csv(path_or_buf = f, sep='\t',
+						header=True, index=False, quotechar='"',
+						line_terminator='\n', escapechar='\\')
 
 def set_arguments():
 	parser = argparse.ArgumentParser('Use an excel file (multiple new users) or the function arguments (one new user at a time) to create new users in the database')
@@ -162,10 +197,10 @@ def set_arguments():
 	parser.add_argument('-u', '--username', help='define the username to be created', type=str, required=False)
 	parser.add_argument('-n', '--name', help='define the name of the user', type=str, required=False)
 	parser.add_argument('-e', '--email', help='define the email of the user being created', type=str, required=False)
-	parser.add_argument('-s', '--second_email', help='define an alternative email for the user', type=str, required=False)
-	parser.add_argument('-p', '--phone', help='define a phone number where the user can be reached', type=str, required=False)
+	parser.add_argument('-s', '--secondary_email', help='define an alternative email for the user', type=str, required=False)
+	parser.add_argument('-p', '--phone_number', help='define a phone number where the user can be reached', type=str, required=False)
 	parser.add_argument('-a', '--affiliation', help="define the user's affiliation (University, Group)", type=str, required=False)
-	parser.add_argument('-i', '--image', help='define path to a picture of the user', type=str, required=False)
+	parser.add_argument('-i', '--image', help='define path to a picture of the user', type=str, default='', required=False)
 
 	return parser
 
@@ -185,7 +220,7 @@ if __name__ == "__main__":
 	if args.file is None and args.username != None:
 		logger.info('Creating user in the database')
 		print('Creating user in the database')
-		create_user_from_command_line(driver, args, expiration=365)
+		create_user_from_command_line(args, expiration=365)
 		print('Done')
 
 
