af3e4d000f969038a7affe9169e03ee8bf1343e1
albsantosdel <alb.santosdel@gmail.com>
2020-02-05 09:12:20 +0100

    solved issues with HMDB parser and improved speed

--- src/graphdb_builder/databases/parsers/hmdbParser.py
+++ src/graphdb_builder/databases/parsers/hmdbParser.py
@@ -33,29 +33,32 @@ def extract_metabolites(config, databases_directory, download = True):
     parentFields = config['HMDB_parentFields']
     structuredFields = config['HMDB_structures']
     with zipfile.ZipFile(fileName, 'r') as zipped:
-        for f in zipped.namelist():
-            data = zipped.read(f).decode('utf-8') 
-            root = etree.fromstring(data)
-            context = etree.iterwalk(root, events=("end",), tag=prefix+"metabolite")
-            for _,elem in context:
-                values = {child.tag.replace(prefix,''):child.text for child in elem.iterchildren() if child.tag.replace(prefix,'') in fields and child.text is not None}
-                for child in elem.iterchildren(): 
-                    if child.tag.replace(prefix,'') in parentFields:
-                        label = child.tag.replace(prefix,'')
-                        values[label] = set()
-                        for intchild in child.iter():
-                            if intchild.text is not None and intchild.text.strip() != "":
-                                if label in structuredFields:
-                                    if intchild.tag.replace(prefix,'') in structuredFields[label]:
-                                        if len(structuredFields[label]) >1:
-                                            values[intchild.tag.replace(prefix,'')] = intchild.text
-                                        else:
-                                            values[label].add(intchild.text) 
-                                elif intchild.tag.replace(prefix,'') in fields and intchild.text:
-                                    values[label].add(intchild.text) 
-                            
-                if "accession" in values:
-                    metabolites[values["accession"]] = values
+        for zfile in zipped.namelist():
+            zipped.extract(member=zfile, path=directory)
+            xfile = os.path.join(directory, zfile)
+            with open(xfile, 'rb') as f:
+                context = etree.iterparse(f, events=("end",), tag=prefix+"metabolite")
+                for _,elem in context:
+                    values = {child.tag.replace(prefix,''):child.text for child in elem.iterchildren() if child.tag.replace(prefix,'') in fields and child.text is not None}
+                    for child in elem.iterchildren(): 
+                        if child.tag.replace(prefix,'') in parentFields:
+                            label = child.tag.replace(prefix,'')
+                            values[label] = set()
+                            for intchild in child.iter():
+                                if intchild.text is not None:
+                                    text = intchild.text
+                                    if text.strip() != "":
+                                        if label in structuredFields:
+                                            if intchild.tag.replace(prefix,'') in structuredFields[label]:
+                                                if len(structuredFields[label]) >1:
+                                                    values[intchild.tag.replace(prefix,'')] = text
+                                                else:
+                                                    values[label].add(text) 
+                                        elif intchild.tag.replace(prefix,'') in fields and text:
+                                            values[label].add(text)
+                    if "accession" in values:
+                        metabolites[values["accession"]] = values
+                
     return metabolites
 
 def build_metabolite_entity(config, databases_directory, metabolites):
@@ -82,7 +85,7 @@ def build_metabolite_entity(config, databases_directory, metabolites):
 def build_relationships_from_HMDB(config, metabolites, mapping):
     relationships = defaultdict(list)
     associations = config['HMDB_associations']
-    for metid in metabolites:p
+    for metid in metabolites:
         for ass in associations:
             ident = ass
             if len(associations[ass]) > 1:
@@ -106,7 +109,7 @@ def build_HMDB_dictionary(databases_directory, metabolites):
     filename = "mapping.tsv"
     outputfile = os.path.join(directory, filename)
     mp.reset_mapping(entity="Metabolite")
-    with open(outputfile, 'w') as out:
+    with open(outputfile, 'w', encoding='utf-8') as out:
         for metid in metabolites:
             if "name" in metabolites[metid]:
                 name = metabolites[metid]["name"]
