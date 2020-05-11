6cd7c9ba3b76e963c542220e5824297d47db1243
Alberto <alb.santosdel@gmail.com>
2020-05-11 13:17:31 +0200

    Made change in SIDER to get all side effects

--- src/graphdb_builder/databases/parsers/siderParser.py
+++ src/graphdb_builder/databases/parsers/siderParser.py
@@ -28,16 +28,16 @@ def parser(databases_directory, drug_source, download=True):
         drug = re.sub(r'CID\d', 'CIDm', data[0])
         se = data[2]
         evidence_from = str(data[3])
-        freq = data[4]
-        lower_bound = data[5]
-        upper_bound = data[6]
+        #freq = data[4]
+        #lower_bound = data[5]
+        #upper_bound = data[6]
         if se.lower() in phenotypemapping and drug in drugmapping:
             for d in drugmapping[drug]:
                 p = phenotypemapping[se.lower()]
-                relationships.add((d, p, "HAS_SIDE_EFFECT", "SIDER", se, evidence_from, freq, lower_bound, upper_bound))
+                relationships.add((d, p, "HAS_SIDE_EFFECT", "SIDER", se, evidence_from))
     associations.close()
 
-    builder_utils.remove_directory(directory)
+    #builder_utils.remove_directory(directory)
 
     return (relationships, header, output_file, drugmapping, phenotypemapping)
 
@@ -67,6 +67,6 @@ def parserIndications(databases_directory, drugMapping, phenotypeMapping, downlo
 
     associations.close()
 
-    builder_utils.remove_directory(directory)
+    #builder_utils.remove_directory(directory)
 
     return (relationships, header, output_file)
